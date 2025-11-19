4303572b82d3d3617867e2364b0be066361b137a
ShreyAC962 <97959780+ShreyAC962@users.noreply.github.com>
2025-11-19 11:46:13 -0800

    nodes

---
+++ nodes.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+from langgraph.graph import MessagesState
+from langgraph.prebuilt import ToolNode
+
+load_dotenv()
+
+from react import llm, tools
+
+
+SYSYEM_MESSAGE="""
+You are a helpful assistant that can use tools to answer questions.
+"""
+
+def run_agent_reasoning(state: MessagesState) -> MessagesState:
+    """
+    Run the agent reasoning node.
+    """
+    response = llm.invoke([{"role": "system", "content": SYSYEM_MESSAGE}, *state["messages"]])
+    return {"messages": [response]}
