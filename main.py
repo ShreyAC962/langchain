78f203de7790337805cf801df390479359ca9966
ShreyAC962 <97959780+ShreyAC962@users.noreply.github.com>
2025-11-19 20:47:57 -0800

    Generated Graph for reflexion-agent

--- main.py
+++ main.py
@@ -1,83 +1,42 @@
-import datetime
+from typing import List
 
-from dotenv import load_dotenv
+from langchain_core.messages import BaseMessage, ToolMessage
+from langgraph.graph import END, MessageGraph
 
-load_dotenv()
+from chains import first_responder, revisor
+from tool_executor import execute_tools
 
-from langchain_core.messages import HumanMessage
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_ollama import ChatOllama
+MAX_ITERATIONS = 2
+builder = MessageGraph()
+builder.add_node("draft", first_responder)
+builder.add_node("execute_tools", execute_tools)
+builder.add_node("revise", revisor)
+builder.add_edge("draft", "execute_tools")
+builder.add_edge("execute_tools", "revise")
 
-from schemas import AnswerQuestion, ReviseAnswer
+
+def event_loop(state: List[BaseMessage]) -> str:
+    count_tool_visits = sum(isinstance(item, ToolMessage) for item in state)
+    num_iterations = count_tool_visits
+    if num_iterations > MAX_ITERATIONS:
+        return END
+    return "execute_tools"
 
 
-
-from langchain_core.output_parsers.openai_tools import (
-    JsonOutputToolsParser,
-    PydanticToolsParser,
+builder.add_conditional_edges(
+    "revise", event_loop, {END: END, "execute_tools": "execute_tools"}
 )
+builder.set_entry_point("draft")
+graph = builder.compile()
+
+print(graph.get_graph().draw_mermaid())
+
+print(graph.get_graph().draw_ascii())
+graph.get_graph().draw_mermaid_png(output_file_path="graph.png")
 
 
-llm = ChatOllama(model="llama3.1:8b-instruct-q4_0", temperature=0)
-
-
-
-parser = JsonOutputToolsParser(return_id=True)
-parser_pydantic = PydanticToolsParser(tools=[AnswerQuestion])
-
-actor_prompt_template = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """You are expert researcher.
-Current time: {time}
-
-1. {first_instruction}
-2. Reflect and critique your answer. Be severe to maximize improvement.
-3. Recommend search queries to research information and improve your answer.""",
-        ),
-        MessagesPlaceholder(variable_name="messages"),
-        ("system", "Answer the user's question above using the required format."),
-    ]
-).partial(
-    time=lambda: datetime.datetime.now().isoformat(),
+res = graph.invoke(
+    "Write about AI-Powered SOC / autonomous soc  problem domain, list startups that do that and raised capital."
 )
-
-
-first_responder_prompt_template = actor_prompt_template.partial(
-    first_instruction="Provide a detailed ~250 word answer."
-)
-
-first_responder = first_responder_prompt_template | llm.bind_tools(
-    tools=[AnswerQuestion], tool_choice="AnswerQuestion"
-)
-
-revise_instructions = """Revise your previous answer using the new information.
-    - You should use the previous critique to add important information to your answer.
-        - You MUST include numerical citations in your revised answer to ensure it can be verified.
-        - Add a "References" section to the bottom of your answer (which does not count towards the word limit). In form of:
-            - [1] https://example.com
-            - [2] https://example.com
-    - You should use the previous critique to remove superfluous information from your answer and make SURE it is not more than 250 words.
-"""
-
-revisor = actor_prompt_template.partial(
-    first_instruction=revise_instructions
-) | llm.bind_tools(tools=[ReviseAnswer], tool_choice="ReviseAnswer")
-
-
-
-
-if __name__ == "__main__":
-    human_message = HumanMessage(
-        content="Write about AI-Powered SOC / autonomous soc  problem domain,"
-        " list startups that do that and raised capital."
-    )
-    chain = (
-        first_responder_prompt_template
-        | llm.bind_tools(tools=[AnswerQuestion], tool_choice="AnswerQuestion")
-        | parser_pydantic
-    )
-
-    res = chain.invoke(input={"messages": [human_message]})
-    print(res)
+print(res[-1].tool_calls[0]["args"]["answer"])
+print(res)
